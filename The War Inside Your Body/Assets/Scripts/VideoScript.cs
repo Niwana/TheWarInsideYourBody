73e58aac9975c04a877ba4006454a2f993f88566
Simon Nielsen <simon.c.nielsen@gmail.com>
2019-10-21 12:53:56 +0200

    Added the intro and outro videos

---
+++ The War Inside Your Body/Assets/Scripts/VideoScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoScript : MonoBehaviour
+{
+
+    public bool PlayIntroVideo = false;
+    public bool PlayOutroVideo = false;
+    public GameObject VideoCanvas;
+    public GameObject IntroVideo;
+    public GameObject OutroVideo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayIntroVideo)
+        {
+            VideoCanvas.SetActive(true);
+            IntroVideo.SetActive(true);
+
+            IntroVideo.GetComponent<UnityEngine.Video.VideoPlayer>().loopPointReached += IntroVideoOver;
+        }
+
+        //Temporary code to test playing outro, instead PlayOutroVideo should be called when appropriate
+        if (PlayOutroVideo)
+        {
+            VideoCanvas.SetActive(true);
+            OutroVideo.SetActive(true);
+
+            OutroVideo.GetComponent<UnityEngine.Video.VideoPlayer>().loopPointReached += OutroVideoOver;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IntroVideoOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        VideoCanvas.SetActive(false);
+        IntroVideo.SetActive(false);
+    }
+
+    void OutroVideoOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        VideoCanvas.SetActive(false);
+        OutroVideo.SetActive(false);
+    }
+
+    void PlayOutrovideo()
+    {
+        VideoCanvas.SetActive(true);
+        OutroVideo.SetActive(true);
+
+        OutroVideo.GetComponent<UnityEngine.Video.VideoPlayer>().loopPointReached += OutroVideoOver;
+    }
+}
